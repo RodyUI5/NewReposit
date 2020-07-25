72b3aeff57b36c09a139603df5493f2f523a66d1
RodyUI5 <rody02ar@hotmail.com>
2020-07-25 22:22:16 +0000

    my first commit local

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NewReposit
+# NewReposit repositorio rrrr
 Nuevo repositorio
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"PrimerProyect/ProyectAPP/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
